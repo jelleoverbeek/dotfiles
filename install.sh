06cea2732c09ab2d13c9d3eaeb21add1e0f984e9
Bart Vosman <bart.vosman@nos.nl>
2019-02-06 11:34:55 +0100

    Make install script fancier

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
+# Symlink files
+echo "⚪️ Starting symlinking"
+
+echo "⚪️ symlinking hyper.js"
 ln -sf ~/.dotfiles/.hyper.js ~/.hyper.js
+
+echo "⚪️ symlinking .zshrc"
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+
+echo "⚪️ symlinking .zshenv"
 ln -sf ~/.dotfiles/zsh/.zshenv ~/.zshenv
 
+echo "✅ Symlinking completed"
+
+# Make zsh default shell
+echo "⚪️ Change default shell to zsh, you password is required!"
 chsh -s /bin/zsh
+
+echo "✅ zsh completed"
