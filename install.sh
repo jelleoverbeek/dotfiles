97dd25d05383c4564dfa92220291f6f8905aba3d
Bart Vosman <bartvosman@barts-mbp.intern.nos.nl>
2019-02-11 15:13:41 +0100

    Add brewfile for installing apps

--- install.sh
+++ install.sh
@@ -17,3 +17,12 @@ echo "⚪️ Change default shell to zsh, you password is required!"
 chsh -s /bin/zsh
 
 echo "✅ zsh completed"
+
+# Install Homebrew apps
+echo "⚪️ Downloading Homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "⚪️ Installing brew apps"
+brew bundle
+
+echo "✅ homebrew done"
